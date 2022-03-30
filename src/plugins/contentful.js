1983fa42f9143b8462f298db7ed5c9fb3adb90ac
Haruto Ikehara <58582197+kduxwr@users.noreply.github.com>
2022-03-30 20:42:01 +0900

    debug

--- src/plugins/contentful.js
+++ src/plugins/contentful.js
@@ -1,10 +1,11 @@
-
 const contentful = require('contentful')
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
+
+console.log(process.env)
 const config = {
   space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
 }
 
 // export `createClient` to use it in page components
