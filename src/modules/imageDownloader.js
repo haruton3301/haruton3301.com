0105b9e5eab5cd08abed1529094b336898e300e1
Haruto Ikehara <58582197+kduxwr@users.noreply.github.com>
2022-04-09 12:43:45 +0900

    a

--- src/modules/imageDownloader.js
+++ src/modules/imageDownloader.js
@@ -1,20 +1,17 @@
 import { JSDOM } from 'jsdom'
 
 module.exports = async function () {
-  this.nuxt.hook('generate:page', async (arg) => {
-    const pattern = /^articles.+$/g
-    // console.log(arg.route)
-    // console.log(arg.route.match(pattern))
-    if (arg.route.match(pattern) != null) {
-      console.log(arg)
-      const dom = new JSDOM(arg.html)
-      const images = dom.window.document.querySelectorAll('img')
-      console.log(JSON.stringify(images))
-    }
-
-    // // arg.html = '変えたよ'
-    // console.log(arg)
-  })
-
-  this.nuxt.hook('generate:done', async () => {})
+  // this.nuxt.hook('generate:page', async (arg) => {
+  //   const pattern = /^articles.+$/g
+  //   // console.log(arg.route)
+  //   // console.log(arg.route.match(pattern))
+  //   if (arg.route.match(pattern) != null) {
+  //     console.log(arg)
+  //     const dom = new JSDOM(arg.html)
+  //     const images = dom.window.document.querySelectorAll('img')
+  //     console.log(JSON.stringify(images))
+  //   }
+  //   // // arg.html = '変えたよ'
+  //   // console.log(arg)
+  // })
 }
