a1b46c075164bdcd74c24c80d05df9ba466c3272
Haruto Ikehara <58582197+kduxwr@users.noreply.github.com>
2022-04-10 10:04:15 +0900

    ダウンローダーを停止

--- src/modules/imageDownloader.js
+++ src/modules/imageDownloader.js
@@ -1,89 +1,89 @@
-import { JSDOM } from 'jsdom'
-import path from 'path'
-import fs from 'fs'
-import axios from 'axios'
-import sharp from 'sharp'
+// import { JSDOM } from 'jsdom'
+// import path from 'path'
+// import fs from 'fs'
+// import axios from 'axios'
+// import sharp from 'sharp'
 
-const srcList = []
-let imagesDirPath
+// const srcList = []
+// let imagesDirPath
 
-module.exports = async function () {
-  const {
-    rootDir,
-    generate: { dir: generateDir },
-  } = this.options
+// module.exports = async function () {
+//   const {
+//     rootDir,
+//     generate: { dir: generateDir },
+//   } = this.options
 
-  this.nuxt.hook('generate:page', async (arg) => {
-    const pattern = /^.articles.+$/g
+//   this.nuxt.hook('generate:page', async (arg) => {
+//     const pattern = /^.articles.+$/g
 
-    if (arg.route.match(pattern) != null) {
-      const dom = new JSDOM(arg.html)
-      const images = dom.window.document.querySelectorAll('article p img')
-      console.log('images')
+//     if (arg.route.match(pattern) != null) {
+//       const dom = new JSDOM(arg.html)
+//       const images = dom.window.document.querySelectorAll('article p img')
+//       console.log('images')
 
-      images.forEach((image) => {
-        const srcUrl = `https:${image.getAttribute('data-src')}`
-        const fileName = srcUrl.match('.+/(.+?)([?#;].*)?$')[1]
-        srcList.push({
-          srcUrl,
-          fileName,
-        })
-      })
-    }
-    console.log(srcList)
-  })
+//       images.forEach((image) => {
+//         const srcUrl = `https:${image.getAttribute('data-src')}`
+//         const fileName = srcUrl.match('.+/(.+?)([?#;].*)?$')[1]
+//         srcList.push({
+//           srcUrl,
+//           fileName,
+//         })
+//       })
+//     }
+//     console.log(srcList)
+//   })
 
-  this.nuxt.hook('generate:done', async () => {
-    imagesDirPath = path.resolve(rootDir, generateDir, 'images')
-    if (!fs.existsSync(imagesDirPath)) {
-      fs.mkdirSync(imagesDirPath)
-    }
+//   this.nuxt.hook('generate:done', async () => {
+//     imagesDirPath = path.resolve(rootDir, generateDir, 'images')
+//     if (!fs.existsSync(imagesDirPath)) {
+//       fs.mkdirSync(imagesDirPath)
+//     }
 
-    const promiseList = []
-    srcList.forEach((src) => {
-      const srcUrl = src.srcUrl
-      const fileName = src.fileName
-      const generate_file_path = path.resolve(
-        rootDir,
-        generateDir,
-        'images',
-        fileName
-      )
+//     const promiseList = []
+//     srcList.forEach((src) => {
+//       const srcUrl = src.srcUrl
+//       const fileName = src.fileName
+//       const generate_file_path = path.resolve(
+//         rootDir,
+//         generateDir,
+//         'images',
+//         fileName
+//       )
 
-      promiseList.push(downloadPromise(srcUrl, generate_file_path))
-    })
+//       promiseList.push(downloadPromise(srcUrl, generate_file_path))
+//     })
 
-    const result = await Promise.all(promiseList)
-    console.log(result)
+//     const result = await Promise.all(promiseList)
+//     console.log(result)
 
-    console.log(srcList)
-  })
+//     console.log(srcList)
+//   })
 
-  const downloadPromise = (srcUrl, filePath) => {
-    return new Promise((resolve, reject) => {
-      if (fs.existsSync(filePath)) {
-        resolve(false)
-      }
+//   const downloadPromise = (srcUrl, filePath) => {
+//     return new Promise((resolve, reject) => {
+//       if (fs.existsSync(filePath)) {
+//         resolve(false)
+//       }
 
-      axios.get(srcUrl, { responseType: 'arraybuffer' }).then(async (res) => {
-        const ext = filePath.split('.').pop()
+//       axios.get(srcUrl, { responseType: 'arraybuffer' }).then(async (res) => {
+//         const ext = filePath.split('.').pop()
 
-        // if (ext === 'gif') {
-        fs.writeFileSync(filePath, new Buffer.from(res.data))
-        resolve(true)
-        // } else {
-        //   sharp(res.data)
-        //     .resize(600)
-        //     .toBuffer()
-        //     .then((data) => {
-        //       fs.writeFileSync(filePath, data)
-        //       resolve(true)
-        //     })
-        //     .catch((err) => {
-        //       resolve(err)
-        //     })
-        // }
-      })
-    })
-  }
-}
+//         // if (ext === 'gif') {
+//         fs.writeFileSync(filePath, new Buffer.from(res.data))
+//         resolve(true)
+//         // } else {
+//         //   sharp(res.data)
+//         //     .resize(600)
+//         //     .toBuffer()
+//         //     .then((data) => {
+//         //       fs.writeFileSync(filePath, data)
+//         //       resolve(true)
+//         //     })
+//         //     .catch((err) => {
+//         //       resolve(err)
+//         //     })
+//         // }
+//       })
+//     })
+//   }
+// }
