7fd932a0f0adac9974cb01294b67ce4f01d58c2c
Haruto Ikehara <58582197+kduxwr@users.noreply.github.com>
2022-04-10 18:53:10 +0900

    prismにC#を適用

--- src/plugins/prism.js
+++ src/plugins/prism.js
@@ -6,5 +6,6 @@ import 'prismjs/plugins/toolbar/prism-toolbar'
 import 'prismjs/plugins/toolbar/prism-toolbar.css'
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
 import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-csharp'
 // import 'prismjs/components/prism-cs' // C#
 export default Prism
